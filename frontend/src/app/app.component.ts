040010ac77be3bfcb65e69c7199a6846c3859692
jamenendez88 <josearmandomenendezclavijo@gmail.com>
2018-11-29 21:16:42 -0800

    Poniendo audio

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,22 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 /* import { Component, OnInit } from '@angular/core'; */
-/* import { FileUploader, FileSelectDirective } from 'ng2-file-upload/ng2-file-upload';
-const URL = 'http://localhost:3000/api/audioClips' */;
+import { FileUploader, FileSelectDirective } from 'ng2-file-upload/ng2-file-upload';
+import * as RecordRTC from 'recordrtc';
+import { DomSanitizer } from '@angular/platform-browser';
+
+/* const URL = 'http://localhost:3000/api/audioClips'; */
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
- /*  public uploader: FileUploader = new FileUploader({ url: URL, itemAlias: 'audio' }); */
+   public uploader: FileUploader = new FileUploader({ url: 'http://localhost:3000/api/audioClips', itemAlias: 'audio' });
 
   ngOnInit() {
-   /*  this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
-    this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
-      console.log('ImageUpload:uploaded:', item, status, response);
-      alert('File uploaded successfully');
-    }; */
+     this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
+     this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
+       console.log('ImageUpload:uploaded:', item, status, response);
+       alert('File uploaded successfully');
+     };
+  }
+  private record;
+  //Will use this flag for detect recording
+  private recording = false;
+  //Url of Blob
+  private url;
+  private error;
+  constructor(private domSanitizer: DomSanitizer) {
+  }
+  sanitize(url: string) {
+    return this.domSanitizer.bypassSecurityTrustUrl(url);
+  }
+  /**
+   * Start recording.
+   */
+  initiateRecording() {
+
+    this.recording = true;
+    let mediaConstraints = {
+      video: false,
+      audio: true
+    };
+    navigator.mediaDevices
+      .getUserMedia(mediaConstraints)
+      .then(this.successCallback.bind(this), this.errorCallback.bind(this));
+  }
+  /**
+   * Will be called automatically.
+   */
+  successCallback(stream) {
+    var options = {
+      mimeType: "audio/wav",
+      numberOfAudioChannels: 1
+    };
+    //Start Actuall Recording
+    var StereoAudioRecorder = RecordRTC.StereoAudioRecorder;
+    this.record = new StereoAudioRecorder(stream, options);
+    this.record.record();
+  }
+  /**
+   * Stop recording.
+   */
+  stopRecording() {
+    this.recording = false;
+    this.record.stop(this.processRecording.bind(this));
+  }
+  /**
+   * processRecording Do what ever you want with blob
+   * @param  {any} blob Blog
+   */
+  processRecording(blob) {
+    this.url = URL.createObjectURL(blob);
+  }
+  /**
+   * Process Error.
+   */
+  errorCallback(error) {
+    this.error = 'Can not play audio in your browser';
   }
 }
