092c43a70556bc91980b52a39b34f563f75b146d
Jose Armando Menendez Clavijo <jdoemax88@gmail.com>
2018-11-30 16:56:12 -0800

    sube

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -18,7 +19,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/components/audios/audios.component.ts
+++ frontend/src/app/components/audios/audios.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import {DataService} from '../../services/data.service';
-import {Observable} from 'rxjs';
+import { DataService } from '../../services/data.service';
+import { Observable } from 'rxjs';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-audios',
   templateUrl: './audios.component.html',
-  styleUrls: ['./audios.component.scss']
+  styleUrls: ['./audios.component.scss'],
+  animations: [
+    trigger('listStagger', [
+      transition('* <=> *', [
+        query(':enter', [
+          style({ opacity: 0, transform: 'translateY(-15px)' }),
+          stagger('50ms',
+            animate('50ms ease-out',
+              style({ opacity: 1, transform: 'translateY(0px)' })))
+        ], { optional: true }),
+        query(':leave', animate('50ms', style({ opacity: 0 })), { optional: true })
+      ])
+    ])
+  ]
 })
 export class AudiosComponent implements OnInit {
 
